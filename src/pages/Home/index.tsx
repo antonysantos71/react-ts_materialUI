b156581c412e7b1fb9a0c7573a2362aaf2222081
Antony <115037549+antonysantos71@users.noreply.github.com>
2024-07-09 17:03:06 +0000

    instalando o materialUI mui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@mui/material"
+
 export const Home = () => {
   return (
     <div>
       <h1>hellou world page home</h1>
+      <Button> click </Button>
     </div>
   )
 }
